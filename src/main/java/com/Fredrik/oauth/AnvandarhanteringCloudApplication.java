0a47f2026509b81cd22f9523b47f0da3eaba37ca
FWiman <frewim@gmail.com>
2025-05-08 12:32:26 +0200

    AnvandarhanteringCloudApplication fix

--- src/main/java/com/Fredrik/oauth/AnvandarhanteringCloudApplication.java
+++ src/main/java/com/Fredrik/oauth/AnvandarhanteringCloudApplication.java
@@ -9,13 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 public class AnvandarhanteringCloudApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
-		System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
-		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-
 		SpringApplication.run(AnvandarhanteringCloudApplication.class, args);
 	}
 }
