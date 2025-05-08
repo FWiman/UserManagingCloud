d94359fce417427c4df89cae7a63a23d732e641e
FWiman <frewim@gmail.com>
2025-05-08 12:46:35 +0200

    AnvandarhanteringCloudApplication fix / pom.xml fix

--- src/main/java/com/Fredrik/oauth/AnvandarhanteringCloudApplication.java
+++ src/main/java/com/Fredrik/oauth/AnvandarhanteringCloudApplication.java
@@ -1,6 +1,5 @@
 package com.Fredrik.oauth;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
